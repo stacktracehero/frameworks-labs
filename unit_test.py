552e8eaf3ee3e243d14c6c7f4b636fdcac8080af
andrii <lukanukandruha@gmail.com>
2026-06-06 12:34:50 +0300

    add github workflow

--- unit_test.py
+++ unit_test.py
@@ -5,5 +5,8 @@ class TestMathOperations(unittest.TestCase):
     def test_add_nums_positive(self):
         self.assertEqual(add_nums(2, 3), 5)
 
+    def test_add_nums_negative(self):
+        self.assertEqual(add_nums(-1, -3), -4)
+
 if __name__ == "__main__":
     unittest.main()
